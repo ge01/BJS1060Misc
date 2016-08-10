f6c648915e0738879b453d43b9637dec9c2ac05d
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-09 20:46:56 -0500

    return a value from a function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Write Reusable JavaScript with Functions
+//Write Reusable JavaScript with Functions
 function reusableFunction() {
   console.log("Heyya, World");
 }
@@ -8,3 +8,22 @@ function reusableFunction(){
   console.log("Hi World");
 }
 reusableFunction();
+
+//Return a Value from a Function with Return
+function minusSeven(num) {
+  return num - 7;
+}
+
+var fromMinusSeven = minusSeven(20);
+console.log(fromMinusSeven);
+console.log(minusSeven(30));
+
+/*Comparison with the Strict Equality Operator
+Strict equality (===) is the counterpart to the equality operator (==).
+Unlike the equality operator, strict equality tests both the data type and
+value of the compared elements.
+Examples
+3 === 3   // true
+3 === '3' // false
+
+*/
