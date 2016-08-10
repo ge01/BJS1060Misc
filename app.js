fcb6d2ee369bd7c79efe520b10999351135b5817
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-09 19:18:40 -0500

    reusable function

---
+++ app.js
@@ -0,0 +1,10 @@
+// Write Reusable JavaScript with Functions
+function reusableFunction() {
+  console.log("Heyya, World");
+}
+reusableFunction();
+
+function reusableFunction(){
+  console.log("Hi World");
+}
+reusableFunction();
